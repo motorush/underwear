ae1c05293f59ea7e849389f9ba2a36ab89d2f313
Archie Semillano <archie_sem@yahoo.com>
2010-07-15 22:26:06 +0800

    config assets true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Boots::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -44,3 +44,4 @@ Boots::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 end
+
